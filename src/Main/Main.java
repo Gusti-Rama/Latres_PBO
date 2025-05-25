b4dcd094305544d150b256de4c31d093e9591976
Gusti-Rama <gustirama309@gmail.com>
2025-05-25 17:36:03 +0700

    tambahan design halaman awal/utama

---
+++ src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+import View.Mahasiswa.ViewData;
+import View.HalamanUtama;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new HalamanUtama().setVisible(true);
+    }
+    
+}
---
+++ src/Model/Dosen/InterfaceDAODosen.java
@@ -0,0 +1,17 @@
+package Model.Dosen;
+
+import java.util.List;
+
+public interface InterfaceDAODosen {
+    // Method untuk memasukkan suatu data mahasiswa
+    public void insert(ModelDosen dosen);
+    
+    // Method untuk mengupdate (mengedit) suatu data mahasiswa
+    public void update(ModelDosen dosen);
+    
+    // Method untuk menghapus suatu data mahasiswa
+    public void delete(int id);
+    
+    // Method untuk mengambil data mahasiswa
+    public List<ModelDosen> getAll();
+}
